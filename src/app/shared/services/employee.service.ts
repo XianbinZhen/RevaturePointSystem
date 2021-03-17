0f450da76dd19dac021cbe3c9ba4d3542d973a8a
XianbinZhen <62436049+XianbinZhen@users.noreply.github.com>
2021-03-17 12:44:28 -0400

    added mock data for employee

--- src/app/shared/services/employee.service.ts
+++ src/app/shared/services/employee.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Employee } from '../models/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
+  employees: Employee[] = [
+    { employeeId: 1, role: "admin", fname: "Adam", lname: "Ranieri", username: "adam", password!: "adam",
+      currentRevaPoints: 9999, allTimeRevaPoints: 9999, batchId: 0}, 
+    { employeeId: 1, role: "admin", fname: "Adam", lname: "Ranieri", username: "adam", password!: "adam",
+      currentRevaPoints: 9999, allTimeRevaPoints: 9999, batchId: 0}, 
+    { employeeId: 1, role: "admin", fname: "Adam", lname: "Ranieri", username: "adam", password!: "adam",
+      currentRevaPoints: 9999, allTimeRevaPoints: 9999, batchId: 0}, 
+    { employeeId: 1, role: "admin", fname: "Adam", lname: "Ranieri", username: "adam", password!: "adam",
+      currentRevaPoints: 9999, allTimeRevaPoints: 9999, batchId: 0}, 
+    { employeeId: 1, role: "admin", fname: "Adam", lname: "Ranieri", username: "adam", password!: "adam",
+      currentRevaPoints: 9999, allTimeRevaPoints: 9999, batchId: 0}, 
+  ];
+
   constructor() { }
+
+  getAllEmployees(): Observable<any> {
+    return of(this.employees.slice());
+  }
+
 }
