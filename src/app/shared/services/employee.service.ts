31c069410efb116293c42345ee85effd09ce2e28
XianbinZhen <62436049+XianbinZhen@users.noreply.github.com>
2021-03-17 14:06:07 -0400

    update mock data

--- src/app/shared/services/employee.service.ts
+++ src/app/shared/services/employee.service.ts
@@ -8,21 +8,21 @@ import { Employee } from '../models/employee';
 export class EmployeeService {
 
   employees: Employee[] = [
-    { employeeId: 1, role: "admin", fname: "Adam", lname: "Ranieri", username: "adam", password!: "adam",
+    { employeeId: 1, role: "admin", fname: "Adam", lname: "Ranieri", username: "adam", password: "adam",
       currentRevaPoints: 9999, allTimeRevaPoints: 9999, batchId: 0}, 
-    { employeeId: 1, role: "admin", fname: "Adam", lname: "Ranieri", username: "adam", password!: "adam",
-      currentRevaPoints: 9999, allTimeRevaPoints: 9999, batchId: 0}, 
-    { employeeId: 1, role: "admin", fname: "Adam", lname: "Ranieri", username: "adam", password!: "adam",
-      currentRevaPoints: 9999, allTimeRevaPoints: 9999, batchId: 0}, 
-    { employeeId: 1, role: "admin", fname: "Adam", lname: "Ranieri", username: "adam", password!: "adam",
-      currentRevaPoints: 9999, allTimeRevaPoints: 9999, batchId: 0}, 
-    { employeeId: 1, role: "admin", fname: "Adam", lname: "Ranieri", username: "adam", password!: "adam",
-      currentRevaPoints: 9999, allTimeRevaPoints: 9999, batchId: 0}, 
+    { employeeId: 2, role: "associate", fname: "Assoc1", lname: "Ranieri", username: "assoc1", password: "adam",
+      currentRevaPoints: 200, allTimeRevaPoints: 600, batchId: 1}, 
+    { employeeId: 3, role: "associate", fname: "Assoc2", lname: "Ranieri", username: "assoc2", password: "adam",
+      currentRevaPoints: 500, allTimeRevaPoints: 500, batchId: 1}, 
+    { employeeId: 4, role: "associate", fname: "Assoc3", lname: "Ranieri", username: "assoc3", password: "adam",
+      currentRevaPoints: 100, allTimeRevaPoints: 300, batchId: 2}, 
+    { employeeId: 5, role: "associate", fname: "Assoc 4", lname: "Ranieri", username: "assoc4", password: "adam",
+      currentRevaPoints: 0, allTimeRevaPoints: 0, batchId: 0}, 
   ];
 
   constructor() { }
 
-  getAllEmployees(): Observable<any> {
+  getAllEmployees(): Observable<Employee[]> {
     return of(this.employees.slice());
   }
 
--- src/app/shared/services/price.service.ts
+++ src/app/shared/services/price.service.ts
@@ -10,15 +10,15 @@ export class PriceService {
   prices: Price[] = [
     // put mock data here
     { priceId: 1, name: 'Ipad', cost: 100, description: "New Ipad", employeeId: 0 },
-    { priceId: 1, name: 'Ipad', cost: 100, description: "New Ipad", employeeId: 0 },
-    { priceId: 1, name: 'Ipad', cost: 100, description: "New Ipad", employeeId: 0 },
-    { priceId: 1, name: 'Ipad', cost: 100, description: "New Ipad", employeeId: 0 },
-    { priceId: 1, name: 'Ipad', cost: 100, description: "New Ipad", employeeId: 0 }
+    { priceId: 2, name: 'Ipad 2', cost: 200, description: "New Ipad 2", employeeId: 1 },
+    { priceId: 3, name: 'Ipad 3', cost: 300, description: "New Ipad 3", employeeId: 0 },
+    { priceId: 4, name: 'Ipad 4', cost: 400, description: "New Ipad 4", employeeId: 0 },
+    { priceId: 5, name: 'Ipad 5', cost: 600, description: "New Ipad 5", employeeId: 0 }
   ]
 
   constructor() { }
 
-  getAllPrice(): Observable<any> {
+  getAllPrice(): Observable<Price[]> {
     return of(this.prices.slice());
   }
 }
