a7acdf7e60bcf23ff5edc5694bfd6fa10b2b36ff
XianbinZhen <62436049+XianbinZhen@users.noreply.github.com>
2021-03-24 15:36:17 -0400

    add loading and error message to assign-batch and give-revature-point pages

--- src/app/views/assign-batch/assign-batch.component.ts
+++ src/app/views/assign-batch/assign-batch.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Employee } from 'src/app/shared/models/employee';
 import { TrainerService } from 'src/app/shared/services/trainer.service';
+import { AppLoaderService } from 'src/app/shared/shared-component/app-loader/app-loader.service';
 
 @Component({
   selector: 'app-assign-batch',
   templateUrl: './assign-batch.component.html',
-  styleUrls: ['./assign-batch.component.scss']
+  styleUrls: ['./assign-batch.component.scss'],
 })
 export class AssignBatchComponent implements OnInit {
   batchId: number = 1;
@@ -13,24 +15,42 @@ export class AssignBatchComponent implements OnInit {
 
   actionType: string = 'Assign';
 
-  constructor(private trainerService: TrainerService) { }
+  constructor(
+    private trainerService: TrainerService,
+    private loader: AppLoaderService,
+    private snackbar: MatSnackBar
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   action(e: any) {
-
-    this.trainerService.getEmployeeById(e).subscribe((res) => {
-      let employee: Employee = res;
-      employee.batchId = this.batchId;
-      this.trainerService.updateEmployee(employee).subscribe((res) => {
-        this.changeValue = !this.changeValue;
-      }, error => {
-        console.log(error);
-      });
-    }, error => {
-      console.log(error);
-    });
+    this.loader.open();
+    this.trainerService.getEmployeeById(e).subscribe(
+      (res) => {
+        let employee: Employee = res;
+        employee.batchId = this.batchId;
+        this.trainerService.updateEmployee(employee).subscribe(
+          (res) => {
+            this.changeValue = !this.changeValue;
+            this.snackbar.open("Employee updated", 'OK', {
+              duration: 3000,
+            });
+            this.loader.close();
+          },
+          (error) => {
+            this.snackbar.open(error?.error?.error, 'error', {
+              duration: 3000,
+            });
+            this.loader.close();
+          }
+        );
+      },
+      (error) => {
+        this.snackbar.open(error?.error?.error, 'error', {
+          duration: 3000,
+        });
+        this.loader.close();
+      }
+    );
   }
-
 }
--- src/app/views/give-revature-point/give-revature-point.component.ts
+++ src/app/views/give-revature-point/give-revature-point.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Employee } from 'src/app/shared/models/employee';
+import { Prize } from 'src/app/shared/models/prize';
 import { TrainerService } from 'src/app/shared/services/trainer.service';
+import { AppLoaderService } from 'src/app/shared/shared-component/app-loader/app-loader.service';
 
 @Component({
   selector: 'app-give-revature-point',
@@ -13,25 +16,43 @@ export class GiveRevaturePointComponent implements OnInit {
   changeValue = false;
 
   actionType: string = 'Give away';
-  constructor(private trainerService: TrainerService) {}
+  constructor(
+    private trainerService: TrainerService,
+    private loader: AppLoaderService,
+    private snackbar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {}
 
   action(e: any) {
-
-    this.trainerService.getEmployeeById(e).subscribe((res) => {
-      let employee: Employee = res;
-      employee.allTimeRevaPoints += this.amount;
-      employee.currentRevaPoints += this.amount;
-      this.trainerService.updateEmployee(employee).subscribe((res) => {
-        console.log(res);
-        this.changeValue = !this.changeValue;
-      }, error => {
-        console.log(error);
-      });
-    }, error => {
-      console.log(error);
-    });
+    this.loader.open();
+    this.trainerService.getEmployeeById(e).subscribe(
+      (res) => {
+        let employee: Employee = res;
+        employee.allTimeRevaPoints += this.amount;
+        employee.currentRevaPoints += this.amount;
+        this.trainerService.updateEmployee(employee).subscribe(
+          (res) => {
+            this.changeValue = !this.changeValue;
+            this.snackbar.open('Employee updated', 'OK', {
+              duration: 3000,
+            });
+            this.loader.close();
+          },
+          (error) => {
+            this.snackbar.open(error?.error?.error, 'error', {
+              duration: 3000,
+            });
+            this.loader.close();
+          }
+        );
+      },
+      (error) => {
+        this.snackbar.open(error?.error?.error, 'error', {
+          duration: 3000,
+        });
+        this.loader.close();
+      }
+    );
   }
-  
 }
